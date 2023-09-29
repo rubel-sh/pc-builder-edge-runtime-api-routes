76bdbdae9af5f8c410ba64cada7e65d2aa13f3be
Rubel Hossain <rubel.hossain0649@gmail.com>
2023-09-29 15:51:04 +0600

    [added] redux state manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
 import PrimaryLayout from "@/components/layout/PrimaryLayout";
+import { store } from "@/redux/store";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
     const getLayout = Component.getLayout || ((page) => page);
     return (
-        <ThemeProvider attribute="class" defaultTheme="system">
-            <PrimaryLayout>{getLayout(<Component {...pageProps} />)}</PrimaryLayout>
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider attribute="class" defaultTheme="system">
+                <PrimaryLayout>{getLayout(<Component {...pageProps} />)}</PrimaryLayout>
+            </ThemeProvider>
+        </Provider>
     );
 }
--- src/pages/builder/index.js
+++ src/pages/builder/index.js
@@ -1,7 +1,10 @@
 import Container from "@/components/CustomUI/Container";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const PcBuilder = () => {
+    const counter = useSelector((state) => state.counter.value);
+    console.log(counter);
     return <Container>PcBuilder</Container>;
 };
 
